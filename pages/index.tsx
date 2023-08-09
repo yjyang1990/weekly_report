f0f6aa2868998fc5e04791eddbcdeede6d75ff42
yjyang1990 <1281074136@qq.com>
2023-08-09 17:43:49 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,7 +110,7 @@ const Home: NextPage = () => {
 
       <a
           className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/guaguaguaxia/weekly_report"
+          href="https://github.com/yjyang1990/weekly_report"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -193,7 +193,7 @@ const Home: NextPage = () => {
                 {t('privacyPolicy1')}
               <a
                 className="text-blue-200 hover:text-blue-400"
-                href="https://github.com/guaguaguaxia/weekly_report/blob/main/privacy.md"
+                href="https://github.com/yjyang1990/weekly_report/blob/main/privacy.md"
                 target="_blank"
                 rel="noopener noreferrer"
               >{' '}{t('privacyPolicy2')}</a>
